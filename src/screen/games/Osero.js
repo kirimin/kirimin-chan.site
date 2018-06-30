04af2eff37fdde311448c870821c503dae83486f
kirimin <cc@kirimin.me>
2018-06-30 13:11:39 +0900

    implements

--- src/screen/games/Osero.js
+++ src/screen/games/Osero.js
@@ -6,6 +6,8 @@ export default class Osero extends Component {
   constructor() {
     super()
     this.state = {
+      onGame: false,
+      finishGame: false,
       player: 1,
       board : [[0, 0, 0, 0, 0, 0, 0, 0], 
               [0, 0, 0, 0, 0, 0, 0, 0], 
@@ -30,27 +32,51 @@ export default class Osero extends Component {
     )
   }
 
-  _onClickSquare(i, j) {
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.finishGame) {
+      alert('ゲーム終了！　黒：' + this._countStones(1) + ', 白：' + this._countStones(2));
+      return
+    }
+    if (!this._canNext()) {
+      this.setState({finishGame: true})
+    }
+  }
+
+  _initBoard() {
+    var trList = [];
+  
+    for(var i = 0; i < 8; i++){
+      const tdList = []
+      for (var j = 0; j < 8; j++) {
+        tdList.push(<td key={i + ',' + j} onClick={this._onClickSquare.bind(this, j, i)}>{this._getStoneState(j, i)}</td>);
+      }
+      trList.push(<tr key={'tr' + i}>{tdList}</tr>)
+    }
+    return trList
+  }
+
+  _onClickSquare(x, y) {
     // 石がおけるか
-    if (!this._canPutStone(i, j)) {
+    if (!this._canPutStone(x, y)) {
       return
     }
     // 石を置く
-    this._putStone(i, j)
+    this._putStone(x, y)
     // 石をひっくり返す
-    this._reverseStones(i, j)
+    this._reverseStones(x, y)
+
     // ターン変更
     this.setState({player: this._getOtherPlayer()})
   }
 
-  _putStone(i, j) {
+  _putStone(x, y) {
     const temp = this.state.board.slice()
-    temp[i][j] = this.state.player
+    temp[x][y] = this.state.player
     this.setState({board: temp})
   }
 
-  _getStoneState(i, j) {
-    return this.state.board[i][j] === 0 ? '　' : this.state.board[i][j] === 1 ? '●' : '◯'
+  _getStoneState(x, y) {
+    return this.state.board[x][y] === 0 ? '　' : this.state.board[x][y] === 1 ? '●' + x + y : '◯' + x + y
   }
 
   _getOtherPlayer() {
@@ -61,35 +87,113 @@ export default class Osero extends Component {
     return this.state.player === 1 ? '黒' : '白'
   }
 
-  _canPutStone(i, j) {
-    if (this.state.board[i][j] !== 0) {
+  _canNext() {
+    for(var i = 0; i < 8; i++){
+      for (var j = 0; j < 8; j++) {
+        if (this._canPutStone(i, j)) {
+          return true
+        }
+      }
+    }
+    return false
+  }
+
+  _countStones(player) {
+    let count = 0
+    for(var i = 0; i < 8; i++){
+      for (var j = 0; j < 8; j++) {
+        if (this.state.board[i][j] === player) {
+          count++
+        }
+      }
+    }
+    return count
+  }
+
+  _canPutStone(x, y) {
+    if (this.state.board[x][y] !== 0) {
       return false
     }
-    // TODO
+    if (this._getCanChangeSquareList(x, y).length === 0) {
+      return false
+    }
     return true
   }
 
-
-  _reverseStones(i, j) {
-    // TODO
-    this._getCanChangeSquareList(i, j).map(it => this._putStone(it.i, it.j))
+  _reverseStones(x, y) {
+    this._getCanChangeSquareList(x, y).map(it => {
+      this._putStone(it.x, it.y)
+    })
   }
 
-  _getCanChangeSquareList(i, j) {
-    // TODO
-    return [{i: i - 1, j: j}]
+  _getCanChangeSquareList(x, y) {
+    return this._searchChangePlacesRight(x, y, this.state.player)
+    .concat(this._searchChangePlacesLeft(x, y, this.state.player))
+    .concat(this._searchChangePlacesBottom(x, y, this.state.player))
+    .concat(this._searchChangePlacesTop(x, y, this.state.player))
   }
 
-  _initBoard() {
-    var trList = [];
-  
-    for(var i = 0; i < 8; i++){
-      const tdList = []
-      for (var j = 0; j < 8; j++) {
-        tdList.push(<td key={i + ',' + j} onClick={this._onClickSquare.bind(this, i, j)}>{this._getStoneState(i, j)}</td>);
+  _searchChangePlacesRight(x, y, color) {
+    const changePlaceList = [];
+
+    for (var i = x + 1; i < 8; i++) {
+      const tempColor = this.state.board[i][y]
+      if (tempColor === 0) {
+        return [];
       }
-      trList.push(<tr key={'tr' + i}>{tdList}</tr>)
-    }
-    return trList
+      if (tempColor === color) {
+        return changePlaceList;
+      }
+      changePlaceList.push({x: i, y: y})
+    };
+    return [];
+  }
+
+  _searchChangePlacesLeft(x, y, color) {
+    const changePlaceList = [];
+
+    for (var i = x - 1; i >= 0; i--) {
+      const tempColor = this.state.board[i][y]
+      if (tempColor === 0) {
+        return [];
+      }
+      if (tempColor === color) {
+        return changePlaceList;
+      }
+      changePlaceList.push({x: i, y: y})
+    };
+    return [];
+  }
+
+  _searchChangePlacesBottom(x, y, color) {
+    const changePlaceList = [];
+
+    for (var i = y + 1; i < 8; i++) {
+      const tempColor = this.state.board[x][i]
+      if (tempColor === 0) {
+        return [];
+      }
+      if (tempColor === color) {
+        return changePlaceList;
+      }
+      changePlaceList.push({x: x, y: i})
+    };
+    return [];
+  }
+
+  _searchChangePlacesTop(x, y, color) {
+    const changePlaceList = [];
+
+    for (var i = y - 1; i >= 0; i--) {
+      const tempColor = this.state.board[x][i]
+      if (tempColor === 0) {
+        return [];
+      }
+      if (tempColor === color) {
+        return changePlaceList;
+      }
+      changePlaceList.push({x: x, y: i})
+    };
+    return [];
   }
 }
