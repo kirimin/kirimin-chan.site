ed573425c8c05b962441dd79b6504db877cf78f4
kirimin <minkarakirimade@gmail.com>
2019-04-29 19:58:40 +0900

    wrote works page

--- src/screen/Works.js
+++ src/screen/Works.js
@@ -29,7 +29,28 @@ export class Works extends Component {
         </section>
         <section className="Works_Body">
           <div>
-            <h1>じゅんびちゅう</h1>
+            <h1>案件募集中</h1>
+            <p>駆け出しVTuberのきりみんちゃんですが、VTuber案件ぼしゅうちゅうです！！！</p>
+            <p>まだ実績がないため、格安価格で案件を受けたいとかんがえています。お気軽にご相談ください！！！</p>
+            <h2>案件の例</h2>
+            <ul>
+              <li>動画でのイベントやサービスなどの宣伝</li>
+              <li>イベントやサービス、企業などとのタイアップ、コラボ</li>
+              <li>イベントなどへの出演、登壇、講演依頼</li>
+            </ul>
+            <h2>きりみんちゃんセールスポイント</h2>
+            <ul>
+              <li>Web業界、特にモバイルエンジニア界隈での高い知名度</li>
+              <li>フリーランスで働く現役Androidエンジニアとしての技術、エンジニアへの理解</li>
+              <li>エンジニアやプログラミングに興味がある学生などのファン層</li>
+            </ul>
+          </div>
+          <div className="Home_Formrun" dangerouslySetInnerHTML={{__html: `<div
+            class="formrun-embed"
+            data-formrun-form="@kirimin-chan"
+            data-formrun-redirect="true">
+          </div>
+          `}}>
           </div>
         </section>
       </div>
